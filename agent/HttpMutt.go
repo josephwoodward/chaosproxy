7ea7298b0c35df222b8b413f27bf09467323bd98
Joseph Woodward <joseph.woodward@xeuse.com>
2017-05-23 10:57:03 +0100

    Work on handler

--- agent/HttpMutt.go
+++ agent/HttpMutt.go
@@ -21,17 +21,29 @@ func Log(configLocation string, port string) {
 
 func setProxy(config configuration.ConfigurationOptions, port string) {
 
-	target := "http://localhost:5000"
-	remote, err := url.Parse(target)
-	if err != nil {
-		panic(err)
-	}
+	//target := "http://localhost:5000"
+	//remote, err := url.Parse(target)
+	//if err != nil {
+	//	panic(err)
+	//}
 
-	proxy := httputil.NewSingleHostReverseProxy(remote)
+	//proxy := httputil.NewSingleHostReverseProxy(remote)
 
 	r := mux.NewRouter()
 	for _, endpoint := range config.Endpoints {
-		r.HandleFunc(endpoint.Path, handler(proxy))
+		//r.HandleFunc(endpoint.Path, handler(proxy))
+		r.HandleFunc(endpoint.Path, func(w http.ResponseWriter, request *http.Request) {
+
+			fullPath := request.Host + request.URL.Path
+			remote, err := url.Parse(fullPath)
+			if err != nil {
+				panic(err)
+			}
+
+			proxy := httputil.NewSingleHostReverseProxy(remote)
+			r.HandleFunc(endpoint.Path, handler(proxy))
+
+		})
 	}
 
 	http.Handle("/", r)
