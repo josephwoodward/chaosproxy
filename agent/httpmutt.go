fb606d406256bf1ad1f65c45d2a687b9a0509f96
Joseph Woodward <joseph.woodward@xeuse.com>
2017-05-24 08:52:11 +0100

    Added logging

--- agent/httpmutt.go
+++ agent/httpmutt.go
@@ -2,9 +2,8 @@ package agent
 
 import (
 	"HttpMutt/configuration"
-	"fmt"
 	"github.com/elazarl/goproxy"
-	"log"
+	"github.com/golang/glog"
 	"net/http"
 	"regexp"
 	"time"
@@ -15,12 +14,13 @@ const delay = 5000
 func Log(configLocation string, port string, outputLog bool) {
 	config, err := configuration.ParseConfig(configLocation)
 	if err != nil {
-		panic(err)
+		glog.Fatal(err)
 	}
 
-	fmt.Println("Starting Proxy")
-	fmt.Println("Listening on port " + port)
+	glog.Error("Hello world!")
+	glog.Infof("Starting proxy, listening on port %s", port)
 	setProxy(config, port)
+
 }
 
 func setProxy(config configuration.ConfigurationOptions, port string) {
@@ -31,7 +31,7 @@ func setProxy(config configuration.ConfigurationOptions, port string) {
 	for _, endpoint := range config.Endpoints {
 		regex, err := regexp.Compile(endpoint.Url)
 		if err != nil {
-			fmt.Printf("Invalid regex format on URL.\n")
+			glog.Warning("Invalid regex format on URL.\n")
 		}
 
 		go proxy.OnRequest(goproxy.DstHostIs(endpoint.Host), goproxy.UrlMatches(regex)).DoFunc(func(req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
@@ -39,11 +39,11 @@ func setProxy(config configuration.ConfigurationOptions, port string) {
 		})
 	}
 
-	log.Fatal(http.ListenAndServe(":"+port, proxy))
+	glog.Fatal(http.ListenAndServe(":"+port, proxy))
 }
 
 func routeFactory(config configuration.Endpoint, req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
-	fmt.Println("Host: " + req.Host)
+	glog.Infof("Matched host '%s'", req.Host)
 	return injectLatency(config, req, ctx)
 }
 
@@ -55,7 +55,7 @@ func blockRequest(r *http.Request, c *goproxy.ProxyCtx) (*http.Request, *http.Re
 }
 
 func injectLatency(config configuration.Endpoint, req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
-	log.Printf("Injecting %d milliseconds latency for %s on path %s", delay, req.Host, req.URL.Path)
+	glog.Infof("Injecting %d milliseconds latency for '%s' on path '%s'", delay, req.Host, req.URL.Path)
 	time.Sleep(time.Millisecond * delay)
 	return req, nil
 }
--- main.go
+++ main.go
@@ -6,19 +6,11 @@ import (
 )
 
 func main() {
+	flag.Parse()
 
 	configLocation := flag.String("o", "./options.json", "Location of config file")
 	portNumber := flag.String("p", "8080", "Port number to listen on")
 	outputLog := flag.Bool("l", false, "Output log to file")
 
-	flag.Parse()
-
 	agent.Log(*configLocation, *portNumber, *outputLog)
-
-	//HttpLog.Log(HttpLog.ConfigurationOptions{
-	//	Port:          *portNumber,
-	//	LogDir:        *logDir,
-	//	RunAsDetached: false,
-	//})
-
 }
